6d8ff4cacf2a91e64ab54d7a869cd8b1b7eb0693
abdullahbakla7323 <153926191+abdullahbakla7323@users.noreply.github.com>
2024-10-25 14:50:18 +0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    int guess;
+    const int secret_number = 3;    
+    do {        
+        printf("Guess a number between 1 and 5: ");
+        scanf("%d", &guess);   
+      
+        if (guess == secret_number) {
+            printf(" You are a genius, You guessed it right.\n");
+        } else {
+            printf(" Unfortunately, you guessed wrong. Try again!\n");
+        }
+    } while (guess != secret_number); 
+
+    return 0;
+}
